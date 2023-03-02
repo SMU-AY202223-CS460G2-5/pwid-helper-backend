3c05f3168933a2adef4ee76aa53518e7635a83a4
iUniquee <idexter.chuaa@gmail.com>
2023-03-02 15:50:01 +0800

    added webhook

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    return "Hello, POST!"
 
 
 if __name__ == "__main__":
