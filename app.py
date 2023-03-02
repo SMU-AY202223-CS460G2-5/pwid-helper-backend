59acb6a9a1a83f17b4e817aea3f6851a75593e5e
iUniquee <idexter.chuaa@gmail.com>
2023-03-02 16:03:30 +0800

    End points for each

--- app.py
+++ app.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    return "Hello, POST!"
+    return "Hello, Webhook!"
+
+@app.route("/health", methods=["POST"])
+def webhook():
+    return "Hello, Health Check!"
+
+@app.route("/rasp", methods=["POST"])
+def webhook():
+    return "Hello, Raspberry Pi!"
 
 
 if __name__ == "__main__":
     app.run()
+
+
