8f2fb43117b87987e0a64fce74eb1c1b3e19d40f
iUniquee <idexter.chuaa@gmail.com>
2023-03-06 17:15:36 +0800

    added validation for webhook

--- app.py
+++ app.py
@@ -1,22 +1,43 @@
-from flask import Flask
+import os
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    return "Hello, Webhook!"
+    # basically what was send to telegram, it sends to somewhere else
+    #token = ""
+    # methodName = "setWebhook"
+    # hostUrl = ""
+    # url = "https://api.telegram.org/bot" + token + "/" + methodName +"?url=" + hostUrl
+
+    result = None
+    if request.is_json:
+        result = request.get_json()
+        
+    else:
+        data = request.get_data()
+        print("Received an invalid Json:")
+        print(data)
+        return jsonify({"code": 400,
+                        # make the data string as we dunno what could be the actual format
+                        "data": str(data),
+                        "message": "Request should be in JSON."}), 400  # Bad Request input
+
+    return result
 
 @app.route("/health", methods=["POST"])
-def webhook():
+def health():
     return "Hello, Health Check!"
 
 @app.route("/rasp", methods=["POST"])
-def webhook():
+def rasp():
     return "Hello, Raspberry Pi!"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000, debug=True)
 
 
