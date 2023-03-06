cca40920e551ef68ba141bc4fa580bfc4f5c84f3
iUniquee <idexter.chuaa@gmail.com>
2023-03-06 21:43:47 +0800

    Added message variable

---
+++  constants.py
@@ -0,0 +1,9 @@
+#Constants for FlashID
+
+startBot = "Thank you for signing up with FlashID. \n We appreciate your time to contribute to the society and your help will greatly help the PWID."
+pwidRequest = "Help has been called. Please hold on tight."
+broadcastRequest = "Hello everyone! \n Help is needed please help\nLocation: {} \nDisabilities: {}"
+acceptRequest = "Thank you for accepting this request.\n More information will be provided below.\nName: {}\nCaregiver Contact Number: {}\nnLocation: {} \nDisabilities: {}\nPlease show your diagram to the PWID for verification"
+pwidResponse = "Do not worry. Help is on the way.\nPlease see your microbit for the same diagram to show your helper."
+broadcastAccepted = "Thank you everyone, this request has been taken and help is already on the way."
+
