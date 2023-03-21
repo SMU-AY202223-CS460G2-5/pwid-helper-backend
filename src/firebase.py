52f78f2fd2f1fcb719380a8c281e4a7c92f20c13
iUniquee <105334333+iUniquee@users.noreply.github.com>
2023-03-21 22:05:21 +0800

    feat: check and update user availability (#6)

--- src/firebase.py
+++ src/firebase.py
@@ -14,3 +14,25 @@ firebase_admin.initialize_app(cred)
 
 # Access Firestore using the default project ID
 db = firestore.client()
+
+def available():
+    result = {}
+    collection_ref = db.collection('users')
+    docs = collection_ref.get()
+
+    for doc in docs:
+        if doc.get("available"):
+            result[doc.get("username")] = doc.to_dict()
+
+
+    return(result)
+
+def change_available(username):
+    doc_ref = db.collection('users').document(username)
+    doc_data = doc_ref.get().to_dict()
+
+    my_field = doc_data["available"]
+    if my_field:
+        doc_ref.update({'available': False})
+    else:
+        doc_ref.update({'available': True})
